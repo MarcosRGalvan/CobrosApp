8bf8edb57be60b3b95cfc0e330038aab1d1148ea
MarcosRGalvan <130708813+MarcosRGalvan@users.noreply.github.com>
2026-05-19 18:46:19 -0600

    Initial Commit

---
+++ CobrosApp/CobrosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobrosAppApp.swift
+//  CobrosApp
+//
+//  Created by Marco Ramirez on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CobrosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
